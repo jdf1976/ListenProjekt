dda8f3f5c2b959395e731af62468a55a6d756b93
Joerg Fluck <jdf1976@gmail.com>
2021-03-31 14:12:32 +0200

    SiteController entfernt

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -3,8 +3,8 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class SiteController extends AbstractController
